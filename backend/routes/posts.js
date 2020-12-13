e01613edc05711862ea35e65bd4f5b21149fc22f
ReneLumiste <lumisterene@gmail.com>
2020-12-13 19:35:17 +0200

    tasks 1, 2, 3 finished (except for that video does not load)

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -5,7 +5,6 @@ const PostModel = require('../models/PostModel');
 
 
 router.get('/', authorize, (request, response) => {
-    // Endpoint to get posts of people that currently logged in user follows or their own posts
     PostModel.getAllForUser(request.currentUser.id, (postIds) => {
         if (postIds.length) {
             PostModel.getByIds(postIds, request.currentUser.id, (posts) => {
@@ -17,54 +16,33 @@ router.get('/', authorize, (request, response) => {
     })
 });
 
-//seda ma ei taipa üldse, kuidas tegema peaks
 router.post('/', authorize,  (request, response) => {
-    let userId = request.currentUser.id;
-    let post = request.body;
-
-    PostModel.getAllForUser(userId, (postIds) => {
-        if (postIds.length) {
-            PostModel.getByIds(postIds, userId, (posts) => {
-                if (!posts.contains(post)) {
-                    PostModel.create(post, newPost => {
-                        posts.push(newPost)
-                        response.status(200).json(posts);
-                    })
-                }
-            })
-        }
-    })
+    if (request.body.text!=null) {
+        PostModel.create(
+            {
+                "userId": request.currentUser.id,
+                "text": request.body.text,
+                "media": request.body.media}, ()=>{})
+        response.json([])
+    }
+    else
+        response.status(400).json()
 });
 
-// selle loogika peaks enam-vähem vist olema, aga ma ei tea, kuidas see päriselt toimima panna!
 router.put('/:postId/likes', authorize, (request, response) => {
-    let userId = Number(request.currentUser.id);
-    let postId = Number(request.params.postId);
-    // let likes = request.body.likes;
-    let liked = request.body.liked;
+    let user = request.currentUser.id
+    let post = request.params.postId
 
-    PostModel.getLikesByUserIdAndPostId(userId, postId, () => {
-        if (liked === false) {
-            PostModel.like(userId, postId, liked => {
-                response.status(200).json(liked);
-            });
-        }
-    })
-});
+    PostModel.like(user, post, ()=>{})
+    response.json([])
+})
 
-//see peaks töötama õigesti! tähendab see toimis eile õhtul ja täna enam ei toimi ://
 router.delete('/:postId/likes', authorize, (request, response) => {
-    let userId = Number(request.currentUser.id);
-    let postId = Number(request.params.postId);
-    // let liked = request.body.liked;
+    let user = request.currentUser.id
+    let post = request.params.postId
 
-    PostModel.getLikesByUserIdAndPostId(userId, postId, (likes) => {
-        if (likes.length > 0) {
-            PostModel.unlike(userId, postId, unliked => {
-                response.status(200).json(unliked);
-            });
-        }
-    })
-});
+    PostModel.unlike(user, post, ()=>{})
+    response.json([])
+})
 
 module.exports = router;
